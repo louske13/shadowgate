5f02ad053eef949e9d0b86d61d1bb9d377e8daee
louske13 <muzuoolivi.fabien@hotmail.fr>
2025-03-29 02:45:29 +0100

    Fix render_template missing for biotrace.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template_string
+from flask import Flask, request, redirect, render_template, render_template_string
 import requests
 import smtplib
 from email.mime.text import MIMEText
